4c6a3b310721ca5cd4b1ad70b0b98a4a21d8bffd
vcu-allens7 <78107104+vcu-allens7@users.noreply.github.com>
2021-05-04 19:01:06 -0400

    Update homework.py

--- spiral2/homework.py
+++ spiral2/homework.py
@@ -1,7 +1,9 @@
 def spiralize(size, n=1):
     #create an empty list
     spiral2 = [] 
-    #populate list with the diagonals of the spiral... with a for loop?
+    for x in range (0,501)
+        #something that chooses only the diagonals
+        spiral2.append(x)
     n = sum(spiral2)
     return_value = n
     return return_value
